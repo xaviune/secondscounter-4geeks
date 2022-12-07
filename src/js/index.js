cd4b4178101416c2481f762a8ee9a79bc1e5cc74
xaviune <xaviune@gmail.com>
2022-12-07 11:29:05 +0000

    Ejercicio funcionando

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,44 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 import "../styles/index.css";
+import propTypes from "prop-types";
+const clockicon = <FontAwesomeIcon icon={faClock}  />;
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter (props){
+    return(
+        <div className="Bigcounter">
+            <div className="calendar">{clockicon} </div>    
+            <div className="six">{props.digitSix % 10}</div>
+            <div className="five">{props.digitFive % 10}</div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitThree % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>        
+        </div>
+    );
+};
+SimpleCounter.PropTypes = {
+    digitSix: propTypes.number,    
+    digitFive: propTypes.number,
+    digitFour: propTypes.number,
+    digitThree: propTypes.number,
+    digitTwo: propTypes.number,
+    digitOne: propTypes.number    
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+    const six = Math.floor(counter/100000);    
+    const five = Math.floor(counter/10000);    
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter ++;
+    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />, document.querySelector("#app"));
+},1000);
+
+
